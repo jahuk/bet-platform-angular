29766240d75af55117652209867c54381a68af4d
Jan Knigawka <jan.knigawka@gmail.com>
2016-12-07 20:32:23 +0100

    add core functionality to match view

--- app/controllers/MatchListCtrl.js
+++ app/controllers/MatchListCtrl.js
@@ -1,5 +1,37 @@
 angular.module('APP')
 
-    .controller('MatchListCtrl', function FixturesUserListCtrl() {
+    .controller('MatchListCtrl', function MatchListCtrl($state, $q, $log, UsersService, FixturesService, PointsService) {
         var matchListCtrl = this;
+        var matchId = $state.params.match;
+
+        $q.all([
+            UsersService.getUsers(),
+            FixturesService.getPureFixtures()
+        ]).then(success, error);
+
+        function success(response) {
+            var users = response[0];
+            var fixtures = response[1];
+
+            var fixture = fixtures[matchId].bet;
+            var matchday = fixtures[matchId].matchday;
+
+            matchListCtrl.users = users;
+
+            _.forEach(users, function(user, index) {
+                var userId = index;
+                var userBets = user.bets;
+                var bet = userBets[matchId].bet;
+
+                matchListCtrl.users[userId].bet = bet;
+                matchListCtrl.users[userId].points = PointsService.getPoints(bet, fixture, matchday);
+            });
+
+            matchListCtrl.result = fixture;
+            matchListCtrl.users = UsersService.sortUsers(matchListCtrl.users);
+        }
+
+        function error(error) {
+            $log.error('ERROR status', error.status);
+        }
     });
